da3839b1215dc77d196f66c7fdf0dcf4d319f4ba
Paulina <paulina.bistula@gmail.com>
2023-08-17 16:55:50 +0200

    Add page navigation and styling to Navbar

--- disney-api/src/components/Navbar/Navbar.jsx
+++ disney-api/src/components/Navbar/Navbar.jsx
@@ -1,5 +1,20 @@
+import { Link, useLocation } from 'react-router-dom';
 import styles from './Navbar.module.css';
 
 export const Navbar = () => {
-	return <div className={styles.navbar}>Navbar</div>;
+	const { pathname } = useLocation();
+
+	return (
+		<div className={styles.navbar}>
+			<Link to="/" className={styles.logo}>
+				<img src="../../../public/disney_logo.png" alt="Logo Disney" className={styles.logoImg} />
+			</Link>
+			<Link
+				to="/favourites"
+				className={pathname === '/favourites' ? styles.activeLink : styles.defaultLink}
+			>
+				My favourite characters
+			</Link>
+		</div>
+	);
 };
