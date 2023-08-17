23f8e9aa5adbb189e00b79df2c7707449cf84807
Paulina <paulina.bistula@gmail.com>
2023-08-17 18:31:18 +0200

    Add fetch data from Disney API

--- disney-api/src/components/Home/Home.jsx
+++ disney-api/src/components/Home/Home.jsx
@@ -1,5 +1,59 @@
+import { useEffect, useState } from 'react';
 import styles from './Home.module.css';
 
 export const Home = () => {
-	return <div className={styles.wrapper}>Home</div>;
+	const [charactersList, setCharactersList] = useState([]);
+
+	const getData = async () => {
+		try {
+			const apiData = await fetch('https://api.disneyapi.dev/character').then((response) =>
+				response.json()
+			);
+			// console.log('API data: ', apiData);
+			// setCharactersList(apiData.data);
+
+			const apiDataNext = await fetch(apiData.info.nextPage).then((response) => response.json());
+			// console.log('API data next: ', apiDataNext);
+
+			setCharactersList([...apiData.data, ...apiDataNext.data]);
+
+			// let prevDataNextPageURL = apiData.info.nextPage;
+			// console.log(prevDataNextPageURL);
+			// console.log(charactersList.length);
+
+			// while (charactersList.length < 100) {
+			// 	const apiDataNext = await fetch(prevDataNextPageURL).then((response) => response.json());
+			// 	// console.log('API data next: ', apiDataNext);
+			// 	setCharactersList((charactersList) => [...charactersList, ...apiDataNext.data]);
+			// 	prevDataNextPageURL = apiDataNext.info.nextPage;
+			// 	// console.log(prevDataNextPageURL);
+			// 	// setCharactersList(apiData.data);
+			// }
+
+			// const apiDataNext = await fetch(apiData.info.nextPage).then((response) => response.json());
+			// console.log('API data next: ', apiDataNext);
+
+			// setCharactersList([...charactersList.data, ...apiDataNext.data]);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	useEffect(() => {
+		getData();
+	}, []);
+
+	console.log(charactersList);
+	console.log(charactersList.length);
+
+	return (
+		<div className={styles.wrapper}>
+			<div className={styles.popular}></div>
+			<div className={styles.characters}>
+				{charactersList
+					? charactersList.map((character) => <li key={character._id}>{character.name}</li>)
+					: null}
+			</div>
+		</div>
+	);
 };
